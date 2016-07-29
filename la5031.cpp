23fde8ce2e3df9e14a4b195399d549ca5354b973
Scottliang <liang199611@outlook.com>
2016-07-29 15:18:02 +0800

    my first commit

---
+++ la5031.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: la5031.cpp
+	> Author:HaoWei 
+	> Mail:liang199611@outlook.com 
+	> Created Time: 2016年07月29日 星期五 15时10分54秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;i++)
+
+int main()
+{
+    freopen("in.txt","r",stdin);
+
+    return 0;
+}
